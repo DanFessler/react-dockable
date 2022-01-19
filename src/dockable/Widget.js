a9b5ae6961d8c37508e58bde8e985767a7e130bb
Dan Fessler <fezzinate@gmail.com>
2022-01-18 18:04:36 -0800

    .

--- src/dockable/Widget.js
+++ src/dockable/Widget.js
@@ -10,11 +10,16 @@ const createWidget = (WrappedComponent, widgetDef) => {
     ...WrappedComponent.defaultProps,
     ...{
       title: widgetDef && widgetDef.title ? widgetDef.title : widgetDef.id,
-      minHeight: widgetDef && widgetDef.minHeight ? widgetDef.minHeight : 100
-    }
+      minHeight: widgetDef && widgetDef.minHeight ? widgetDef.minHeight : 100,
+    },
   };
 
   return Widget;
 };
+
+// Not sure if I need a HOC anymore, so this is a wrapper component for widgets
+export function Widget({ id, title = id, actions, minHeight = 100, children }) {
+  return children;
+}
 
 export default createWidget;
