fe58d83d6a4baf8f76b4dc54c200ceaabcdaae8c
Dan Fessler <fezzinate@gmail.com>
2025-05-16 16:09:11 -0700

    preparing for package deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ tsconfigPath: "./tsconfig.app.json", rollupTypes: true }),
+  ],
+  build: {
+    lib: {
+      entry: "./lib/dockable/index.ts",
+      name: "react-dockable",
+      fileName: "react-dockable",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
