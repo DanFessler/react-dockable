0998b8c6b59c9e9f678c1406abcea49343dde52a
fezz <fezzinate@gmail.com>
2025-05-18 21:24:17 -0700

    reverting the demo page

--- src/App.tsx
+++ src/App.tsx
@@ -12,66 +12,14 @@ function App() {
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Dockable.Root
-        onChange={(layout) => console.log(JSON.stringify(layout, null, 2))}
-      >
-        <Dockable.Tab id="view-1" name="Left">
-          Left Component
-        </Dockable.Tab>
-        <Dockable.Panel size={3}>
-          <Dockable.Tab id="view-2" name="Top">
-            Top Component
-          </Dockable.Tab>
-          <Dockable.Window>
-            <Dockable.Tab id="view-3" name="Tab 1">
-              Bottom Component 1
-            </Dockable.Tab>
-            <Dockable.Tab id="view-4" name="Tab 2">
-              Bottom Component 2
-            </Dockable.Tab>
-          </Dockable.Window>
-        </Dockable.Panel>
+      <Dockable.Root layout={layout} onChange={setLayout}>
+        {views}
       </Dockable.Root>
     </div>
   );
 }
 
 function createView(id: string, name: string) {
-  return (
-    <Dockable.Tab id={id} name={name}>
-      view
-    </Dockable.Tab>
-  );
+  return <Dockable.Tab id={id} name={name}></Dockable.Tab>;
 }
 export default App;
-
-const layout = [
-  {
-    type: "Window",
-    id: "window-0",
-    children: ["view-1"],
-    size: 1,
-    selected: "view-1",
-  },
-  {
-    type: "Panel",
-    id: "panel-3",
-    children: [
-      {
-        type: "Window",
-        id: "window-1",
-        children: ["view-2"],
-        size: 1,
-        selected: "view-2",
-      },
-      {
-        type: "Window",
-        id: "window-2",
-        children: ["view-3", "view-4"],
-        size: 1,
-        selected: "view-3",
-      },
-    ],
-    size: 3,
-  },
-];
