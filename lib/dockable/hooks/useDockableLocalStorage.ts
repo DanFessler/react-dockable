d1e6a46179886e830d610458fc306047f347d869
Dan Fessler <fezzinate@gmail.com>
2025-05-19 12:39:10 -0700

    improving useDockableLocalStorage hook

--- lib/dockable/hooks/useDockableLocalStorage.ts
+++ lib/dockable/hooks/useDockableLocalStorage.ts
@@ -5,6 +5,9 @@ import type { LayoutNode } from "../utils/serializeLayout";
 export function useDockableLocalStorage(version: number) {
   const savedLayout = localStorage.getItem("layout");
   const parsedLayout = savedLayout ? JSON.parse(savedLayout) : undefined;
+
+  // console.log(parsedLayout, parsedLayout && parsedLayout.version === version);
+
   const [layout, setLayout] = useState<LayoutNode[]>(
     parsedLayout && parsedLayout.version === version
       ? parsedLayout.layout
@@ -12,11 +15,17 @@ export function useDockableLocalStorage(version: number) {
   );
 
   useEffect(() => {
+    // if we have a version mismatch, we dont want to save the layout
+    if (!parsedLayout || parsedLayout.version !== version) {
+      localStorage.removeItem("layout");
+      return;
+    }
+
     localStorage.setItem(
       "layout",
       JSON.stringify({ version: version, layout: layout })
     );
-  }, [layout]);
+  }, [layout, version, parsedLayout]);
 
   return { layout, setLayout };
 }
--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ import { Dockable, useDockableLocalStorage } from "../lib/dockable";
 function App() {
   const { layout, setLayout } = useDockableLocalStorage(3);
 
-  const viewCount = 6;
+  const viewCount = 2;
   const views = Array.from({ length: viewCount }, (_, i) =>
     createView(`view-${i + 1}`, `View ${i + 1}`)
   );
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Dockable.Root layout={layout} onChange={setLayout}>
-        {views}
-      </Dockable.Root>
+      <Dockable.Root layout={layout}>{views}</Dockable.Root>
     </div>
   );
 }
